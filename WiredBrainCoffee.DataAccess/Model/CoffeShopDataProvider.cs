e2fed83c27e8f501dcad804fa7871e6279255a6b
Matias <mnfredes@gmail.com>
2022-10-17 16:11:34 -0300

    Add data access to sln

---
+++ WiredBrainCoffee.DataAccess/Model/CoffeShopDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace WiredBrainCoffee.DataAccess.Model
+{
+    public class CoffeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop() { Location = "Frankfurt", BeansInStockInKg = 107 };
+            yield return new CoffeeShop() { Location = "Freiburg", BeansInStockInKg = 23 };
+            yield return new CoffeeShop() { Location = "Munich", BeansInStockInKg = 56 };
+        }
+    }
+}
